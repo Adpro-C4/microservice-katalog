5388ef86f05bb678472c53643a2a87ab2115eadf
Arya KD <arya.kdaniswara@gmail.com>
2024-04-23 00:55:13 +0700

    [REFACTOR] fix html pages error

--- src/main/java/com/adpro/katalog/model/Product.java
+++ src/main/java/com/adpro/katalog/model/Product.java
@@ -4,12 +4,12 @@ import lombok.Setter;
 @Getter @Setter
 public class Product {
     private String id;
-    private String name;
-    private String description;
-    private int price;
-    private int discount;
-    private String brand;
-    private String category;
-    private String image;
-    private int quantity;
+    private String name = "";
+    private String description = "";
+    private int price = 0;
+    private int discount = 0;
+    private String brand = "";
+    private String category = "";
+    private String image = "";
+    private int quantity = 0;
 }
