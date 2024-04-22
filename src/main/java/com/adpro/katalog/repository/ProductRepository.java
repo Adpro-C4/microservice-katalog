445f62b8fc5258db4cdbe2541d8cba0b24c97ec8
Arya KD <arya.kdaniswara@gmail.com>
2024-04-23 01:08:35 +0700

    [GREEN] make findById and findAll function

--- src/main/java/com/adpro/katalog/repository/ProductRepository.java
+++ src/main/java/com/adpro/katalog/repository/ProductRepository.java
@@ -6,17 +6,30 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    private static int productCount = 0;
 
     public Product create(Product product) {
+        product.setId(Integer.toString(productCount)); // make product id
+        productCount++;
         productData.add(product);
         return product;
     }
 
-    public  Iterator<Product> findAll() {
-        return  productData.iterator();
+    public Iterator<Product> findAll() {
+        return productData.iterator();
+    }
+
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (productId.equals(product.getId())) {
+                return product;
+            }
+        }
+        throw new NoSuchElementException("Product not found with ID: " + productId);
     }
 }
