363b29661a3cade1c25af09e06046fe26ff22188
Arya KD <arya.kdaniswara@gmail.com>
2024-05-09 20:19:40 +0700

    add RabbitMQ

---
+++ src/main/java/com/adpro/katalog/model/dto/ProductDTO.java
@@ -0,0 +1,23 @@
+package com.adpro.katalog.model.dto;
+
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductDTO implements Serializable{
+    private String productId;
+    private String name;
+    private int quantity;
+    private double price;
+    public ProductDTO(){
+
+    }
+    // constructor, getters, and setters
+}
