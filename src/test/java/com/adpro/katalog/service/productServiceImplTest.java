a1270ff516408d535ff8f3093e45acaddcdfe595
Arya KD <arya.kdaniswara@gmail.com>
2024-05-15 16:20:38 +0700

    [RED] Test websocket when edited product is empty

--- src/test/java/com/adpro/katalog/service/productServiceImplTest.java
+++ src/test/java/com/adpro/katalog/service/productServiceImplTest.java
@@ -10,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -40,6 +42,28 @@ class productServiceImplTest {
         when(productRepository.save(any(Product.class))).thenReturn(product);
 
         productService.create(product);
+
+        ArgumentCaptor<String> messageCaptor = ArgumentCaptor.forClass(String.class);
+        verify(productWebSocketHandler).sendMessageToAll(messageCaptor.capture());
+
+        String expectedMessage = "Produk dengan id 1 kosong";
+        assertEquals(expectedMessage, messageCaptor.getValue());
+    }
+
+    @Test
+    void testEditProductToZeroQuantity() {
+        Product product = new Product();
+        product.setId(1L);
+        product.setQuantity(10);
+
+        Product updatedProduct = new Product();
+        updatedProduct.setId(1L);
+        updatedProduct.setQuantity(0);
+
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        when(productRepository.save(any(Product.class))).thenReturn(updatedProduct);
+
+        productService.edit(updatedProduct);
 
         ArgumentCaptor<String> messageCaptor = ArgumentCaptor.forClass(String.class);
         verify(productWebSocketHandler).sendMessageToAll(messageCaptor.capture());
