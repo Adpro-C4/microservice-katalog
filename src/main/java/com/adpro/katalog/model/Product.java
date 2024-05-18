cfa60730db9cf9f55aa2fa228b54a677c3100316
Arya KD <arya.kdaniswara@gmail.com>
2024-05-18 16:41:51 +0700

    [Green] Implement Builder Build Pattern to Product Model

--- src/main/java/com/adpro/katalog/model/Product.java
+++ src/main/java/com/adpro/katalog/model/Product.java
@@ -23,4 +23,80 @@ public class Product {
     private String category = "";
     private String image = "";
     private int quantity = 0;
+
+    // Private constructor for Builder use
+    private Product(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.description = builder.description;
+        this.price = builder.price;
+        this.discount = builder.discount;
+        this.brand = builder.brand;
+        this.category = builder.category;
+        this.image = builder.image;
+        this.quantity = builder.quantity;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name = "";
+        private String description = "";
+        private int price = 0;
+        private int discount = 0;
+        private String brand = "";
+        private String category = "";
+        private String image = "";
+        private int quantity = 0;
+
+        public Builder() {}
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder price(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder discount(int discount) {
+            this.discount = discount;
+            return this;
+        }
+
+        public Builder brand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public Builder category(String category) {
+            this.category = category;
+            return this;
+        }
+
+        public Builder image(String image) {
+            this.image = image;
+            return this;
+        }
+
+        public Builder quantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+    }
 }
